2bee490208174c29a7f1806ba186304926e1c1ea
Timothy Andrus <timothy.andrus@wgu.edu>
2025-05-02 14:05:15 -0600

    added background and changed cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class AppComponent {
-  title = 'Timothy Andrus';
+  title = 'Leadership in Technology During Uncertain Times';
 }
